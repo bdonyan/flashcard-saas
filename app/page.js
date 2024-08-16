fa0519d59916935747bd9a338706890580bbf1fa
bdonyan <brandon_yan123@yahoo.com>
2024-08-16 14:52:52 -0700

    added stripe payments, flashcard visuals, result page

--- app/page.js
+++ app/page.js
@@ -1,3 +1,4 @@
+'use client'
 import Image from "next/image";
 import getStripe from '@/utils/get-stripe'
 import { SignedIn, SignedOut, UserButton } from '@clerk/nextjs'
@@ -6,6 +7,34 @@ import Head from 'next/head'
 
 
 export default function Home() {
+
+  const handleSubmit = async () => {
+    const checkoutSession = await fetch('/api/checkout_session', {
+      method: 'POST',
+      headers: {
+        origin: 'http://localhost:3000',
+      },
+    })
+
+    const checkoutSessionJson = await checkoutSession.json()
+    console.log(checkoutSessionJson); 
+
+    if (checkoutSession.statusCode === 500) {
+      console.error(checkoutSessionJson.message)
+      return
+    }
+
+    console.log(checkoutSessionJson.id);
+
+    const stripe = await getStripe()
+    const {error} = await stripe.redirectToCheckout({
+      sessionId: checkoutSessionJson.id
+    })
+
+    if (error) {
+      console.warn(error.message)
+    }
+  }
   return (
     <Container maxWidth="lg">
       <Head>
@@ -122,7 +151,7 @@ export default function Home() {
                 {' '}
                 Unlimited flashcards and storage, with priority support
               </Typography>
-              <Button variant="contained" color="primary" sx={{mt: 2}}>
+              <Button variant="contained" color="primary" sx={{mt: 2}} onClick={handleSubmit}>
                 Choose Pro
               </Button>
             </Box>
